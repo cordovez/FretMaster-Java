cb34b657a25eb2b76453611a1247fca825500db5
Juan Carlos Cordovez-Mantilla <cordovez@el-cordovez.com>
2024-10-11 11:36:17 +0200

    Initial commit
    Reviewing the FretboardMaster app created originally in Python.

---
+++ src/Fretboard.java
@@ -0,0 +1,34 @@
+public class Fretboard {
+    final static String[][] NOTES = {
+            // Fret 1
+            {"F", "Bb", "Eb", "Ab", "C", "f"},
+            // Fret 2
+            {"Gb", "B", "E", "A", "Db", "g"},
+            // Fret 3
+            {"G", "C", "F", "Bb", "D", "ab"},
+            // Fret 4
+            {"Ab", "Db", "Gb", "B", "Eb", "a"},
+            // Fret 5
+            {"A", "D", "G", "C", "E", "bb"},
+            // Fret 6
+            {"Bb", "Eb", "Ab", "Db", "F", "b"},
+            // Fret 7
+            {"B", "E", "A", "D", "Gb", "c"},
+            // Fret 8
+            {"C", "F", "Bb", "Eb", "G", "db"},
+            // Fret 9
+            {"Db", "Gb", "B", "E", "Ab", "d"},
+            // Fret 10
+            {"D", "G", "C", "F", "A", "eb"},
+            // Fret 11
+            {"Eb", "Ab", "Db", "Gb", "Bb", "e"},
+            // Fret 12
+            {"E", "A", "D", "G", "B", "e"},
+            // Fret 13
+            {"F", "Bb", "Eb", "Ab", "C", "f"},
+            // Fret 14
+            {"Gb", "B", "E", "A", "Db", "g"},
+            // Fret 15
+            {"G", "C", "F", "Bb", "D", "ab"},
+    };
+}
